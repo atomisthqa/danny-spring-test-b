b65d8a1187d38624104fc19b83964806f08b5b51
Danny Smith <danny@atomist.com>
2018-10-05 18:49:57 +0100

    Initial commit from Atomist

---
+++ src/main/java/main/DannySpringTestBApplication.java
@@ -0,0 +1,12 @@
+package main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DannySpringTestBApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DannySpringTestBApplication.class, args);
+	}
+}
